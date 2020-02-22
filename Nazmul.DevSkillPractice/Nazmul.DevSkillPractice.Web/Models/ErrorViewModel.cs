b07c9d069d967a407c30ceaca9fda6c56d014714
Md. Nazmul Huda <nazmulhd10@gmail.com>
2020-02-22 18:43:55 +0600

    Adding File with ASP.Net Core Web App 3

---
+++ Nazmul.DevSkillPractice/Nazmul.DevSkillPractice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nazmul.DevSkillPractice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
