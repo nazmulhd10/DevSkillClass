e2accfc0b5d7c3987c464c8eb87a9bac462d04d8
Md. Nazmul Huda <nazmulhd10@gmail.com>
2020-02-28 03:23:18 +0600

    Add Client Part to Front End

--- Nazmul.AddingWebTemplate/Nazmul.AddingWebTemplate.Web/Startup.cs
+++ Nazmul.AddingWebTemplate/Nazmul.AddingWebTemplate.Web/Startup.cs
@@ -64,8 +64,11 @@ namespace Nazmul.AddingWebTemplate.Web
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
-                    name: "default",
+                    name: "areas",
                     pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
